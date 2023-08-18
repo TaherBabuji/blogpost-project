04ce2b3f782549a3a2128de6f1cf0847094b636a
TaherBabuji <121304971+TaherBabuji@users.noreply.github.com>
2023-08-18 11:46:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 const expressSession = require('express-session')
+app.set("trust proxy", 1);
 app.use(expressSession({
     secret: 'keyboard cat',
     proxy: true,
