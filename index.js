f02b6f590f5ddba97ce9e7447a8d14b4c4386d11
TaherBabuji <121304971+TaherBabuji@users.noreply.github.com>
2023-09-07 20:14:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
-app.use("*", (req, res, next) => {
-    if (req.session && req.session.userId) {
-        loggedIn = req.session.userId;
-    } else {
-        loggedIn = null;
-    }
-    next();
-});
+// app.use("*", (req, res, next) => {
+//     if (req.session && req.session.userId) {
+//         loggedIn = req.session.userId;
+//     } else {
+//         loggedIn = null;
+//     }
+//     next();
+// });
 
 const expressSession = require('express-session')
 app.enable('trust proxy')
