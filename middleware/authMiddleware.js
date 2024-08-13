4886da71ccad4a44a4d38bc1c2bf028f84a1bd1e
TaherBabuji <121304971+TaherBabuji@users.noreply.github.com>
2024-08-13 23:39:43 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,8 +3,6 @@ const User = require('../models/user.js')
 module.exports = async (req, res, next) => {
     try {
         const user = await User.findById({ _id: req.session.userId })
-        console.log(req.session.userId)
-        console.log(user)
         if (!user) {
             console.log("No user founded")
             return res.redirect('/')
